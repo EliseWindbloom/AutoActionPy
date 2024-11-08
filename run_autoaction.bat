41cee841f61cfcd9866ab73a6ba09a22ddf3f2aa
EliseWindbloom <109670213+EliseWindbloom@users.noreply.github.com>
2024-11-08 07:28:49 -0500

    Update run_autoaction.bat

--- run_autoaction.bat
+++ run_autoaction.bat
@@ -1,3 +1,24 @@
+@echo off
 call venv\Scripts\activate.bat
-python autoaction.py
+
+call :filedialog file
+if "%file%"=="" (
+    echo No file selected. Exiting...
+    exit /b
+)
+if /i not "%file:~-4%"==".txt" (
+    echo Selected file is not a .txt file. Exiting...
+    exit /b
+)
+echo Selected file is: "%file%"
+python autoaction.py "%file%"
 pause
+exit /b
+
+:filedialog :: &file
+setlocal 
+set dialog="about:<input type=file id=FILE><script>FILE.click();new ActiveXObject
+set dialog=%dialog%('Scripting.FileSystemObject').GetStandardStream(1).WriteLine(FILE.value);
+set dialog=%dialog%close();resizeTo(0,0);</script>"
+for /f "tokens=* delims=" %%p in ('mshta.exe %dialog%') do set "file=%%p"
+endlocal & set %1=%file%
